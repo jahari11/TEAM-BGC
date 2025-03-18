d407edb3a33b83085b16022c7978a860cd452841
Jahari <jahari.crawford.young@gmail.com>
2025-03-18 18:50:02 -0400

    event ba

--- pages/events.js
+++ pages/events.js
@@ -1,12 +1,8 @@
-import TeamForm from "components/TeamForm";
-import Under18Waiver from "components/Under18Waiver";
-import VendorForm from "components/VendorForm";
-import WaiverForm from "components/WaiverForm";
 
 export default function Events() {
     return (
         <div className="min-h-screen flex items-center justify-center">
-            <WaiverForm />
+            Events TBA
         </div>
     )
 }
